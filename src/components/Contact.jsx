61a1fa2db2def94c6f0358b3f48ec7c4ac5ae93f
Panna Szűcs <szucspann@gmail.com>
2022-12-14 20:04:41 +0100

    final touches

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>
-      <p><img className="svg" src="github.svg" alt="github" />: <a href="https://github.com/Viserya11" target="_blank"> viserya11 </a></p>
-      <p><img src="" alt="" /></p>
-      <p><img className='svg' src="resume.svg" alt="" /><a href=""></a></p>
+    <div className='contactcontainer'>
+      <h1>Let's connect!</h1>
+      <p><img className="contsvg" src="github.svg" alt="github" />: <a href="https://github.com/Viserya11" target="_blank"> viserya11 </a></p>
+      <p><img className="contsvg" src="email.svg" alt="" /> <a href="https://mail.google.com/mail/?view=cm&fs=1&to=szucspann@outlook.com"> szucspann@outlook.com </a></p>
+      <p><img className='contsvg' src="resume.svg" alt="" /><a href="cv_panna_szucs.pdf" download> Download CV </a></p>
     </div>
   )
 }
