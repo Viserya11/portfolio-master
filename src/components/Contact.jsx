3a61b6c7c84ca6d8a2ff90b178256b559a7feb33
Panna Szűcs <szucspann@gmail.com>
2022-12-14 19:42:30 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <p><img className="svg" src="github.svg" alt="github" />: <a href="https://github.com/Viserya11" target="_blank"> viserya11 </a></p>
+      <p><img src="" alt="" /></p>
+      <p><img className='svg' src="resume.svg" alt="" /><a href=""></a></p>
+    </div>
   )
 }
